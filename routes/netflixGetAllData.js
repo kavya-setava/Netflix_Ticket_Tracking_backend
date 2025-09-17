98ff1443e726b6106939365167949722d25c13df
Kavya Setava <kavya.setava@mediamint.com>
2025-09-17 22:50:50 +0530

    removed 2 apis and add 4 apis (db and sheet separate)

--- routes/netflixGetAllData.js
+++ routes/netflixGetAllData.js
@@ -9,12 +9,10 @@ router.post("/qmdata", getNetflixTickets.qmdata);
 
 
 router.get("/getNetflixTickets", getNetflixTickets.getNetflixTickets);  // using this api
-router.put("/updateTicketByKey/:ticketKey", getNetflixTickets.updateTicketByKey);  // using this api
 router.put("/updateTicketByKey_DB/:ticketKey", getNetflixTickets.updateTicketByKey_DB);  // using this api
 router.put("/updateTicketByKey_Sheet/:ticketKey", getNetflixTickets.updateTicketByKey_Sheet);  // using this api
 router.get('/dropdown', getNetflixTickets.CMTicketsFilterOptions); // uisng this api not using 
 router.get('/getCMs', getNetflixTickets.getCMs); // uisng this api
-router.put('/update-backup-cm', getNetflixTickets.updateBackupCM);
 router.put('/updateBackupCM_Sheet', getNetflixTickets.updateBackupCM_Sheet);
 router.put('/updateBackupCM_DB', getNetflixTickets.updateBackupCM_DB);
 // router.get("/getTicketSLA",getNetflixTickets.getTicketSLA);
