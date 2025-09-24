88804a183c4b058c78fa17ee3e2f0e3aefaba35c
Kavya Setava <kavya.setava@mediamint.com>
2025-09-24 23:44:13 +0530

    ticket enable/disable

--- controllers/netflixGetAllController.js
+++ controllers/netflixGetAllController.js
@@ -999,6 +999,8 @@ exports.updateTicketByKey_DB = async (req, res) => {
     const { status, startTime, endTime, SLA ,asap} = req.body;
 
     console.log("🔄 Updating ticket in DB:", ticketKey);
+    console.log('...........',req.body);
+    
 
     const existingTicket = await NetflixTicket.findOne({ ticketKey: ticketKey.trim() });
 
@@ -1006,8 +1008,8 @@ exports.updateTicketByKey_DB = async (req, res) => {
       return res.status(404).json({ success: false, error: 'Ticket not found' });
     }
 
-    if (status === undefined && startTime === undefined && endTime === undefined && SLA === undefined,  asap === undefined) {
-      return res.status(400).json({ success: false, error: 'No fields to update' });
+      if ([status, startTime, endTime, SLA, asap].every(field => field === undefined)) {
+      return res.status(400).json({ success: false, error: "No fields to update" });
     }
     
     
